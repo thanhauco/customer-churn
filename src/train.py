72b48d9a64299ca50d29a6472c9ff0477ac8731b
Thanh Vu <thanhauco@gmail.com>
2022-10-10 08:24:00 -0700

    Add MLflow Tracking

--- src/train.py
+++ src/train.py
@@ -1,6 +1,2 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-def train(X, y):
-    model = RandomForestClassifier()
-    model.fit(X, y)
-    return model
+import mlflow
+mlflow.log_param('param', 1)
